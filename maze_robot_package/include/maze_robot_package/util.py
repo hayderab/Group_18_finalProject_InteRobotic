da18fae9a68a4427618891821224ed32261bb187
George Karanasios <kar.giorgos7@hotmail.com>
2021-12-03 06:12:35 -0800

    Uploaded converter grid method

--- maze_robot_package/include/maze_robot_package/util.py
+++ maze_robot_package/include/maze_robot_package/util.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import random
 def getName():
 	return "maze_robot_package"
--- maze_robot_package/src/subscriber_node.py
+++ maze_robot_package/src/subscriber_node.py
@@ -1,11 +1,15 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import rospy
 
 from nav_msgs.msg import OccupancyGrid, Odometry
 
+
 def callback(data):
     rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)  # print grids occupancy
-    # rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.pose.pose.position.x)  # print robot x position
+
+    # convertGridCoordinates
+    # printRobotCoordinates(data)
+
 
 
 def listener():
@@ -16,6 +20,27 @@ def listener():
 
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
+
+
+def convertGridCoordinates(data):
+    width = 0
+    while width < data.info.width:
+        height = 0
+        while height < data.info.height:
+            if data.data[height * data.info.width + width] > 0:
+                x = width * data.info.resolution + data.info.resolution / 2
+                y = height * data.info.resolution + data.info.resolution / 2
+                print("(" + "x[" + str(width) + "]" + ", " + "y[" + str(height) + "]" + "): " + str(x) + ", " + str(y))
+            height += 1
+        width += 1
+
+
+def printRobotCoordinates(data):
+    print("x:")
+    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.pose.pose.position.x)  # print robot x position
+
+    print("y:")
+    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.pose.pose.position.y)  # print robot y position
 
 
 if __name__ == '__main__':
