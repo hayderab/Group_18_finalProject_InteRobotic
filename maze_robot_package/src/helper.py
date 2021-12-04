110eb8e44a1cbc34b7d0091fcc98d341bd3ae833
George Karanasios <kar.giorgos7@hotmail.com>
2021-12-04 08:33:48 -0800

    Small change

--- maze_robot_package/src/helper.py
+++ maze_robot_package/src/helper.py
@@ -10,7 +10,8 @@ def save_to_text_file(givenDict, filename):
     try:
         textfile = open(os.path.join(desktop_dir, filename), "w+")
         for coordinates, values in givenDict.items():
-            textfile.write("(x, y)" + "[" + "x-value, " + "y-value, " + "cell value, " + "reward value" + "]" + "\n")
+            textfile.write(
+                "(x_index, y_index)" + "[" + "x, " + "y, " + "cell value, " + "reward value" + "]" + "\n")
             textfile.write(str(coordinates) + str(values) + "\n")
             textfile.write(" " + "\n")
         textfile.close()
@@ -21,33 +22,33 @@ def save_to_text_file(givenDict, filename):
 
 
 # Returns a dictionary with coordinates, grade_cell value and reward
-def create_dict_grid(dict_grid, width, height, x, y, grid_cell):
+def create_dict_grid(dict_grid, width_index, height_index, x, y, grid_cell):
     if grid_cell > 0:  # for obstacles
-        dict_grid[width, height] = [x, y, grid_cell, -1]  # reward = -1
+        dict_grid[width_index, height_index] = [x, y, grid_cell, -1]  # reward = -1
     elif grid_cell == -1:  # for unknown cells
-        dict_grid[width, height] = [x, y, grid_cell, 0]  # reward = 0
-    elif grid_cell == 0:  # for unknown cells
+        dict_grid[width_index, height_index] = [x, y, grid_cell, 0]  # reward = 0
+    else:  # for known cells
         if (8 <= x <= 10) and (9 <= y <= 11):  # for the target goal cell (I chose for now the centre of the maze)
-            dict_grid[width, height] = [x, y, grid_cell, 10]  # reward = 10
+            dict_grid[width_index, height_index] = [x, y, grid_cell, 10]  # reward = 10
         else:
-            dict_grid[width, height] = [x, y, grid_cell, 0]  # reward = 0
+            dict_grid[width_index, height_index] = [x, y, grid_cell, 0]  # reward = 0
     return dict_grid
 
 
 # Returns and saves a dictionary of the maze occupancy grid with the coordinates (x, y) and reward of every cell
 def cell_coordinates_reward(data):
-    width = 0
+    width_index = 0
     dict_grid = {}
-    while width < data.info.width:
-        height = 0
-        while height < data.info.height:
-            grid_cell = data.data[height * data.info.width + width]
+    while width_index < data.info.width:
+        height_index = 0
+        while height_index < data.info.height:
+            grid_cell = data.data[height_index * data.info.width + width_index]
             # if grid_cell > 0:  # only for the obstacles
-            x = width * data.info.resolution + data.info.resolution / 2
-            y = height * data.info.resolution + data.info.resolution / 2
-            dict_grid = create_dict_grid(dict_grid, width, height, x, y, grid_cell)
-            height += 1
-        width += 1
+            x = width_index * data.info.resolution + data.info.resolution / 2  # coordinate x of the current gird cell
+            y = height_index * data.info.resolution + data.info.resolution / 2  # coordinate y of the current grid cell
+            dict_grid = create_dict_grid(dict_grid, width_index, height_index, x, y, grid_cell)
+            height_index += 1
+        width_index += 1
     save_to_text_file(dict_grid, 'results.txt')
 
     return dict_grid
