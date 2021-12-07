702062d44bb1dcab7d14157b792947957a1c7dc2
HuwRanSmith <huwranscombesmith@gmail.com>
2021-12-07 15:08:28 +0000

    Added Policy update function

--- maze_robot_package/src/PolicyIteration.py
+++ maze_robot_package/src/PolicyIteration.py
@@ -1,53 +1,58 @@
+def policy_generate(dicts_grid, width_lgth, height_lgth):
+    # Stores the dictionary with policy directions
+    new_dicts_grid = {}
 
-from helper import *
+    # Loop through all coords
+    for y in range(0,height_lgth):
+        for x in range(0,width_lgth):
+            
+            # Reset at the start of each loop
+            reward = -10000000
+            # Direction the policy is pointing for given coord
+            p_direction = "self"
 
+            # Check 4 surrounding coords for highest reward
+            # Includes logic to prevent going off the grid
+            if not((x+1) >= width_lgth) and (dicts_grid[(x+1,y)][2] > reward):
+                reward = dicts_grid[(x+1,y)][2]
+                p_direction = "left"
 
+            if not((y-1) < 0) and (dicts_grid[(x,y-1)][2] > reward):
+                reward = dicts_grid[(x,y-1)][2]
+                p_direction = "down"
+            
+            if not((x-1) < 0) and (dicts_grid[(x-1,y)][2] > reward):
+                reward = dicts_grid[(x-1,y)][2]
+                p_direction = "right"
 
+            if not((y+1) >= height_lgth) and (dicts_grid[(x,y+1)][2] > reward):
+                reward = dicts_grid[(x,y+1)][2]
+                p_direction = "up"
 
+            # Obtain the value list for current coord to change and
+            # insert into new dictionary
+            value = dicts_grid[(x,y)]
 
+            # Insert the correct policy direction
+            if p_direction == "self":
+                value[3] = (x,y)
+                new_dicts_grid[(x,y)] = value
 
+            elif p_direction == "left":
+                value[3] = (x+1,y)
+                new_dicts_grid[(x,y)] = value
+            
+            elif p_direction == "down":
+                value[3] = (x,y-1)
+                new_dicts_grid[(x,y)] = value
 
+            elif p_direction == "right":
+                value[3] = (x-1,y)
+                new_dicts_grid[(x,y)] = value
 
-def Q(s, a):
-    """
-    Input sate, action
-    Return optimal policy
-    """
-    pass
-
-def P(s, a):
-    """
-    Inputs State and Action
-    return porobality of ending up in the new state
-    """
-    pass
-
-def policyIteration(S, A, P, R):
-    """
-    Inputs:
-       S is the set of all states
-       A is the set of all actions
-       P is state transition function specifying P(s′∣s,a)
-       R is a reward function R(s,a) 
-    Return:
-        Optimal policy pi       
-    """
-    # actionAarray = [a for a in S]
-    # isChange = False
-    # V = [s for s in S]
+            elif p_direction == "up":
+                value[3] = (x,y+1)
+                new_dicts_grid[(x,y)] = value
     
-    # policy = [ ] 
-
-    # while True:
-    #     isChange = True
-    #     V[s] = R(S,A) + gamma * sum()
-    #     for  s in S:
-    #         QBest = V[s]
-    #         for a in A:
-    #             pass
-                
-    #     if isChange == True:
-    #         break  
-
-    # return policy  
-    pass
+    return new_dicts_grid
+            
